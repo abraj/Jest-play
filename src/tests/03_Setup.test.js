fe35d43ffa6a80fcb1cdf0899cda28675bc609dd
Abhishek Raj <abhi@raj.me>
2018-10-02 15:55:07 +0530

    Setup and Teardown

---
+++ src/tests/03_Setup.test.js
@@ -0,0 +1,20 @@
+// NOTE: beforeEach, afterEach, beforeAll, afterAll can handle asynchronous code
+// So, they can either take a 'done' parameter or just return a promise
+
+beforeAll(() => console.log('1 - beforeAll'));
+afterAll(() => console.log('1 - afterAll'));
+
+beforeEach(() => console.log('1 - beforeEach'));
+afterEach(() => console.log('1 - afterEach'));
+
+test('', () => console.log('1 - test'));
+
+describe('Scoped / Nested block', () => {
+  beforeAll(() => console.log('2 - beforeAll'));
+  afterAll(() => console.log('2 - afterAll'));
+
+  beforeEach(() => console.log('2 - beforeEach'));
+  afterEach(() => console.log('2 - afterEach'));
+
+  test('', () => console.log('2 - test'));
+});
